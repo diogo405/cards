5f54606d37ac7b72c12139efb835ffadd2c1c381
Diogo Goncalves <diogo@ticketmates.com.au>
2018-11-09 16:25:33 +1000

    First commit

---
+++ src/components/Card.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="card">
+	</div>
+</template>
+
+<script>
+export default {
+  name: "card"
+};
+</script>
+
+<style lang="scss">
+.card {
+  background: black;
+  width: calc(100% / 4 - 50px);
+  height: 200px;
+  margin-top: 50px;
+  cursor: pointer;
+}
+</style>
---
+++ src/views/Game.vue
@@ -0,0 +1,44 @@
+<template>
+	<div class="game">
+		<div class="game__card-container">
+			<Card v-for="card in cards" :key="card.id"/>
+		</div>
+	</div>
+</template>
+
+<script>
+import Card from "@/components/Card.vue";
+
+export default {
+  name: "game",
+  components: {
+    Card
+  },
+  data() {
+    return {
+      // prettier-ignore
+      cards: [
+      {id: 1, status: "back"}, 
+      {id: 2, status: "back"},
+      {id: 3, status: "back"},
+      {id: 4, status: "back"},
+      {id: 5, status: "back"},
+      {id: 6, status: "back"},
+      {id: 7, status: "back"},
+      {id: 8, status: "back"},
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+.game {
+}
+.game__card-container {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  padding: 5%;
+}
+</style>
